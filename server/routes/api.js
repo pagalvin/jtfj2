c9d5bf35a4f17deb163f18da7ff935d9ccdfdeb1
Paul Galvin <galvin.paul@gmail.com>
2017-08-22 21:43:44 -0400

    Substantial effort to DRY up the code.

--- server/routes/api.js
+++ server/routes/api.js
@@ -42,41 +42,24 @@ router.get('/ping', (req, res) => {
     res.status(200).json({from: "api.js: ping under node", answer: "ping!"});
 })
 
-router.get('/KnowledgeDomains', (req, res) => {
+const getAllItems = (collectionName) => {
+  
+  return new Promise( 
+    (resolve, reject) => {
 
-  var cursor = db.collection('KnowledgeDomains').find();
+      var cursor = db.collection(collectionName).find();
 
-  // console.log(`api.js: /KnowledgeDomains: cursor:`, cursor);
+      db.collection(collectionName).find().toArray((err, results) => {
 
-  db.collection(`KnowledgeDomains`).find().toArray((err, results) => {
+        console.log(`api.js: getAllItems: successfully executed a find().`);
 
-    console.log(`api.js: /KnowledgeDomains: KnowledgeDomains: successfully executed a find().`);
+        resolve(results);
+      });
+    }
+  )
 
-    res.status(200).json(
-      {
-        from: "api.js: /KnowledgeDomains under node",
-        status: "Retrieved all Knowledge Domains from Mongo.",
-        requestBody: req.body,
-        dbResult: results,
-        KnowledgeDomains: results
-      }
-    );
-  });
+}
 
-});
-
-// Get all posts
-router.get('/posts', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
-});
 
 router.post('/pingpost', (req, res) => {
     res.status(200).json(
@@ -150,6 +133,64 @@ const saveExistingItem = (collection, updateWhich, itemToSave) => {
   })
 }
 
+
+function insertNewItemIntoCollection(collectionName, itemToInsert /* req.body */) {
+
+  return new Promise(
+  
+    (resolve, reject) => {
+  
+      console.log(`api.js: insertNewItemIntoCollection: processing a request:`, itemToInsert);
+
+      console.log(`api.js: insertNewItemIntoCollection: Awaiting insert new item.`);
+
+      insertNewItem(db.collection(collectionName), itemToInsert).then(
+        (insertResult) => {
+
+          console.log(`api.js: insertNewItemIntoCollection: New item inserted, id:`, insertResult.ops[0]._id)
+
+          itemToInsert.UniqueID = insertResult.ops[0]._id;
+
+          saveExistingItem(db.collection(collectionName), { _id: itemToInsert.UniqueID }, itemToInsert).then(
+            (updateResults) => {
+
+              console.log(`api.js: /KnowledgeDomains/new: updated item with unique ID, result:`, updateResults)
+
+              resolve(insertResult);
+            }
+          )
+
+        })
+    })
+
+}
+
+
+router.get('/KnowledgeDomainsOLD', (req, res) => {
+
+  var cursor = db.collection('KnowledgeDomains').find();
+
+  // console.log(`api.js: /KnowledgeDomains: cursor:`, cursor);
+
+  getAllItems('KnowledgeDomains').then( 
+    (allItems) => {
+
+    console.log(`api.js: /KnowledgeDomains: KnowledgeDomains: successfully executed a find().`);
+
+    res.status(200).json(
+        {
+          from: "api.js: /KnowledgeDomains under node",
+          status: "Retrieved all Knowledge Domains from Mongo.",
+          requestBody: req.body,
+          dbResult: allItems,
+          KnowledgeDomains: allItems
+        }
+      );
+    }
+  )
+});
+
+/*
 router.post('/KnowledgeDomains/new', (req, res) => {
 
     console.log(`api.js: /KnowledgeDomains/new: Got a request:`, req.body);
@@ -173,13 +214,13 @@ router.post('/KnowledgeDomains/new', (req, res) => {
             
             res.status(200).json(insertResult);
           }
-        )
-
+        ) 
       }
     )
 });
+*/
 
-router.delete('/KnowledgeDomains/:kdID/delete', 
+router.delete('/KnowledgeDomain/:kdID/deleteOLD', 
   (req, res) => {
     console.log(`api.js: /KnowledgeDomains/delete: Got a request params:`, req.params);
 
@@ -196,7 +237,9 @@ router.delete('/KnowledgeDomains/:kdID/delete',
   }
 );
 
-router.post('/KnowledgeDomains/:kdID/update', (req, res) => {
+
+
+router.post('/KnowledgeDomain/:kdID/updateOLD', (req, res) => {
 
     console.log(`api.js: /KnowledgeDomains/new: Got a request params:`, req.params);
 
@@ -215,20 +258,169 @@ router.post('/KnowledgeDomains/:kdID/update', (req, res) => {
       }
     )
 
-
-    // collection.updateOne({UniqueID: req.params.kdID}, req.body).then(
-    //   (result) => {
-    //     res.status(200).json(
-    //       {
-    //         from: `api.js: KnowledgeDomains/${req.params.kdID}/update`,
-    //         summaryResult: "Successfully updated item with UniqueID: [" + req.param.kdID + "].", 
-    //         requestBody: req.body,
-    //         dbResult: result
-    //       });
-    //     }
-    // );
-
 });
+
+
+// router.get('/Facts', (req, res) => {
+
+//   var cursor = db.collection('Facts').find();
+
+//   getAllItems('Facts').then( 
+//     (allItems) => {
+
+//     console.log(`api.js: /Facts: Facts: successfully executed a find().`);
+
+//     res.status(200).json(
+//         {
+//           from: "api.js: /Facts under node",
+//           status: "Retrieved all Facts from Mongo.",
+//           requestBody: req.body,
+//           dbResult: allItems,
+//           KnowledgeDomains: allItems
+//         }
+//       );
+//     }
+//   )
+// });
+
+const getInsertNewItemMount = (mountName, collection) => {
+  return () => {
+
+    router.post(mountName, (req, res) => {
+      console.log(`api.js: ${mountName}: Got a request and awaiting insert operation. req.body:`, req.body);
+
+      insertNewItemIntoCollection(collection, req.body).then(
+        (insertResult) => {
+          console.log(`api.js: ${mountName}: Insert completed successfully, result:`, insertResult);
+          res.status(200).json(insertResult);
+        }
+      );
+    });
+  }
+}
+
+const getAllItemsMount = (mountName, collectionName) => {
+
+  return () => {
+
+    router.get(mountName, (req, res) => { 
+
+      var cursor = db.collection(collectionName).find();
+
+      // console.log(`api.js: /KnowledgeDomains: cursor:`, cursor);
+
+      getAllItems(collectionName).then(
+        (allItems) => {
+
+          console.log(`api.js: getAllItemsCount</${mountName}>: successfully executed a find().`);
+
+          res.status(200).json(
+            {
+              from: `api.js: getAllItemsMount<${mountName}>: from node.`,
+              status: "Retrieved all items from Mongo.",
+              requestBody: req.body,
+              dbResult: allItems,
+              Items: allItems
+            }
+          );
+        }
+      )
+    });
+  }
+
+}
+
+const getUpdateItemMount = (mountName, collectionName) => {
+
+  // '/KnowledgeDomain/:kdID/update'
+
+  return () => {
+    router.post(mountName, (req, res) => {
+
+        const ll = `api.js: getUpdateItemMount<${mountName}>:`;
+
+        console.log(`${ll} Got a request with params:`, req.params);
+
+        const collection = db.collection(collectionName);
+
+        req.body.DateModified = new Date();
+
+        console.log(`${ll} Saving an item:`, req.body);
+
+        saveExistingItem(collection, {_id: ObjectId(req.body.UniqueID)}, req.body).then(
+          (result) => {
+
+            console.log(`${ll} Successfully saved an item:`, req.body);
+            
+            res.status(200).json(result);
+          }
+        )
+
+    });
+  }
+
+}
+
+const getDeleteItemRoute = (mountName, collectionName) => {
+
+  return () => {
+
+    router.delete(mountName,
+      (req, res) => {
+
+        const ll = `api.js: getDeleteItemRoute<${mountName}: `;
+
+        console.log(`${ll} Entering, will delete item with ID: ${req.params.ID}.`);
+
+        const collection = db.collection(collectionName);
+
+        deleteOneExistingItem(collection, { _id: ObjectId(req.params.ID) }).then(
+          (deleteResults) => {
+
+            console.log(`${ll} successfully deleted the item, results:`, deleteResults);
+
+            res.status(200).json(deleteResults);
+          }
+        )
+      }
+    );
+
+  }
+}
+
+const allCrudMounts = () => {
+  const mountInstructions = [
+    {
+      baseMountName: 'KnowledgeDomain',
+      dbCollectionName: 'KnowledgeDomains'
+    },
+    {
+      baseMountName: 'Fact',
+      dbCollectionName: 'Facts'
+    },
+    {
+      baseMountName: 'QuizTemplate',
+      dbCollectionName: 'QuizTemplates'
+    },
+    {
+      baseMountName: 'QuizInstance',
+      dbCollectionName: 'QuizInstances'
+    }
+  ];
+
+  return mountInstructions;
+}
+
+allCrudMounts().forEach( (mountInstructions) => {
+
+    console.log(`api.js: initializing CRUD mounts: /${mountInstructions.baseMountName}.`);
+    getInsertNewItemMount(`/${mountInstructions.baseMountName}/new`,mountInstructions.dbCollectionName)();
+    getAllItemsMount(`/${mountInstructions.baseMountName}s`, mountInstructions.dbCollectionName)(); // Pluralize base mount name on getAll
+    getUpdateItemMount(`/${mountInstructions.baseMountName}/:id/update`, mountInstructions.dbCollectionName)();
+    getDeleteItemRoute(`/${mountInstructions.baseMountName}/:ID/delete`, mountInstructions.dbCollectionName)();
+    
+})
+
 
 const findItemByID = (collection, ID) => {
   return new Promise((resolve, reject) => {
