e49c81236d03123421d9df488431da8480a1e55a
Paul Galvin <galvin.paul@gmail.com>
2017-08-22 21:47:09 -0400

    Leverage mongo server.

--- src/app/Admin/Facts/Facts.Service.ts
+++ src/app/Admin/Facts/Facts.Service.ts
@@ -1,9 +1,12 @@
-﻿import { Functionals } from '../../Framework/Util/Functionals';
+﻿import { AbstractItem, IAbstractItem } from '../../Framework/Data Structures/AbstractItem';
+import { IEntityMetadata } from '../../Framework/Data Structures/IEntityMetadata';
+import { Functionals } from '../../Framework/Util/Functionals';
 import { RecordIDsService } from "../../Framework/Data Services/RecordIDsService";
 import { IFactItem, FactItem } from "./FactModel";
 import { ConsoleLog } from '../../Framework/Logging/ConsoleLogService';
 import { AbstractAngularService } from '../../Framework/Data Structures/AbstractAngularService'
 import { Injectable } from '@angular/core';
+import { MongoService } from '../../Framework/Data Services/MongoData.service';
 
 "use strict";
 
@@ -19,7 +22,8 @@ export class FactsService extends AbstractAngularService {
 
     constructor(
         private cLog: ConsoleLog,
-        private recordsService: RecordIDsService) {
+        private recordsService: RecordIDsService,
+        private mongoService: MongoService) {
 
         super();
 
@@ -36,7 +40,7 @@ export class FactsService extends AbstractAngularService {
         return this._loadAllFactsPromise;
     }
 
-    public async deleteFactByUniqueID(forID: string): Promise<boolean> {
+    public async deleteFactByUniqueIDOLD(forID: string): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
 
@@ -48,26 +52,26 @@ export class FactsService extends AbstractAngularService {
         })
     }
 
-    // public GetFactByUniqueIDng1(forID: string): Promise<IFactItem> {
+    
+    public async deleteFactByUniqueID(theFactID: string): Promise<boolean> {
+        
+        return new Promise<boolean>( async (resolve, rejectO) => {
 
-    //     this.cLog.debug(`FactsService: GetFactByID: loading a fact, [${forID}].`);
+            const ll = "FactsService: deleteFactByID: ";
 
-    //     this._loadAllFactsPromise.then(
-    //         () => {
-    //             this.cLog.debug(`FactsService: GetFactByID: all facts:`, this._allFacts);
-    //             deferred.resolve(<jtfj.Entities.IFactItem>jtfj.Framework.Functionals.getEntityByUniqueID(forID, this._allFacts));
-    //         },
-    //         (errorDetails) => {
-    //             this.cLog.debug(`GetFactByID: Failed to load the facts database, error details:`, errorDetails);
-    //             deferred.reject({
-    //                 msg: "FactsService: GetFactByID: Failed to retrieve the fact.", errorDetails: errorDetails
-    //             });
-    //         }
-    //     );
+            this.cLog.info(`${ll} Entering. Will delete a fact with ID [${theFactID}].`);
 
-    //     return deferred.promise;
+            const itemToDelete = await this.getFactByUniqueID(theFactID);
 
-    // }
+            const deleteResult = await this.mongoService.deleteItemG<FactItem>(itemToDelete);
+
+            this.cLog.info(`KD.Service: deleteKowledgeDomainByID: Item successfully deleted, updating all KDs.`);
+            
+            this._allFacts = Functionals.filterOutEntityByUniqueID(theFactID, this._allFacts);
+            
+            resolve(true);
+        })
+    }
 
     public async getFactByUniqueID(forID: string): Promise<FactItem> {
 
@@ -90,35 +94,8 @@ export class FactsService extends AbstractAngularService {
 
     }
 
-    // public SaveFactng1(theFact: jtfj.Entities.IFactItem): ng.IPromise<boolean> {
-
-    //     this.cLog.debug(`FactsService: SaveFact: Entering. before saving, all facts and fact to save:`, {
-    //         presave: this._allFacts, factToSave: theFact
-    //     });
-
-    //     const deferred = this.$q.defer<boolean>();
-
-    //     if (!F.IsIDAssigned(theFact)) {
-    //         this._recordsService.GetUniqueID().then(
-    //             (newID) => {
-    //                 theFact.UniqueID = newID;
-    //                 this._allFacts = F.getMergedCollection(this._allFacts, theFact);
-    //                 this._persistFacts();
-    //                 deferred.resolve(true);
-    //             }
-    //         );
-    //     }
-    //     else {
-    //         this._allFacts = F.getMergedCollection(this._allFacts, theFact);
-    //         this._persistFacts();
-    //         deferred.resolve(true);
-    //     }
-
-    //     return deferred.promise;
-
-    // }
-
-    public async saveFact(theFact: IFactItem): Promise<boolean> {
+    
+    public async saveFactOld(theFact: IFactItem): Promise<boolean> {
 
         this.cLog.debug(`FactsService: SaveFact: Entering. before saving, all facts and fact to save:`, {
             presave: this._allFacts, factToSave: theFact
@@ -146,6 +123,43 @@ export class FactsService extends AbstractAngularService {
 
     }
 
+    public async saveFact(theFact: FactItem): Promise<boolean> {
+
+        return new Promise<boolean>(
+            async (resolve, reject) => {
+
+                const ll = "FactService: saveFAct: ";
+
+                this.cLog.info(`${ll} Entering, will save a fact:`, { gct: theFact });
+                this.cLog.info(`${ll} is id assigned?:`, Functionals.isIDAssigned(theFact));
+
+                if (!Functionals.isIDAssigned(theFact)) {
+
+                    this.cLog.debug(`${ll} No ID assigned, will create a new record on the mongo side.`);
+
+                    const savedFact = await this.mongoService.createItemG<FactItem>(theFact);
+
+                    this._allFacts = Functionals.getMergedCollection(this._allFacts, theFact);
+
+                    this.cLog.info(`${ll} saved fact:`, { sf: savedFact, all: this._allFacts });
+
+                    resolve(true);
+                }
+                else {
+                    this.cLog.info(`${ll} Already has an ID, no need to get a new one.`);
+
+                    const updatedFact = await this.mongoService.updateItemG(theFact);
+                    this.cLog.info(`${ll}: updated domain:`, updatedFact)
+
+                    this._allFacts = Functionals.getMergedCollection(this._allFacts, theFact);
+                    resolve(true);
+                }
+
+            }
+        )
+    }
+
+
     private async persistFacts(): Promise<boolean> {
 
         this.cLog.debug(`Facts.Service: persistFacts: Entering.`);
@@ -163,7 +177,7 @@ export class FactsService extends AbstractAngularService {
 
     }
 
-    private async loadAllFactsAsync(): Promise<boolean> {
+    private async loadAllFactsAsyncOLD(): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
 
@@ -189,10 +203,31 @@ export class FactsService extends AbstractAngularService {
             }
         });
     }
+   
+    private async loadAllFactsAsync(): Promise<boolean> {
+        
+        return new Promise<boolean> (async (resolve, reject) => {
+            
+            const ll = "FactService: loadAllFactsAsync: ";
+            
+            const allMongoFacts = await this.mongoService.getAllItemsG<FactItem>(FactItem.GetEntityMetadata());
+            
+            this.cLog.info(`${ll} got some facts:`, allMongoFacts);
 
-    public Create20RandomFacts() {
+            if (allMongoFacts) {
+                this._allFacts = allMongoFacts.map((aFact) => {
+                    return FactItem.cleanNulls(aFact);
+                });
+            }
+            else {
+                this._allFacts = [];
+            }
 
+            this.cLog.debug(`FactsService: _loadAllFacts: resolving with true.`);
+            
+            resolve(true);
 
+        });
 
     }
 
