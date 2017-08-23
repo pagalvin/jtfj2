a6150c69e296c644cf271d7fd0c3d62847fa0bc8
Paul Galvin <galvin.paul@gmail.com>
2017-08-22 21:48:16 -0400

    Use mongo service.

--- src/app/Admin/QuizTemplates/QT.Service.ts
+++ src/app/Admin/QuizTemplates/QT.Service.ts
@@ -1,11 +1,14 @@
-﻿import { Injectable } from '@angular/core';
-
+﻿import { KnowledgeDomainsService } from '../KnowledgeDomains/KD.Service';
+import { Injectable, OnInit } from '@angular/core';
 import { RecordIDsService } from '../../Framework/Data Services/RecordIDsService';
 import { AbstractAngularService } from '../../Framework/Data Structures/AbstractAngularService';
 import { Functionals } from '../../Framework/Util/Functionals';
 import { ConsoleLog } from '../../Framework/Logging/ConsoleLogService';
 import { IQuizTemplateItem, QuizTemplateItem } from './QT.Model';
- 
+import { MongoService } from '../../Framework/Data Services/MongoData.service';
+import { AbstractItem, IAbstractItem } from '../../Framework/Data Structures/AbstractItem';
+import { FactsService } from '../Facts/Facts.Service';
+
 "use strict";
 
 @Injectable()
@@ -16,18 +19,21 @@ export class QuizTemplatesService extends AbstractAngularService {
     private allQuizTemplates: IQuizTemplateItem[];
     public get AllQuizTemplates(): IQuizTemplateItem[] { return this.allQuizTemplates; }
 
-    private loadAllQuizTemplatesPromise: Promise<boolean>;
+    private loadAllQuizTemplatesPromise: Promise<boolean> = null;
 
     constructor(
         private clog: ConsoleLog,
-        private recordsService: RecordIDsService) {
+        private recordsService: RecordIDsService,
+        private factsService: FactsService,
+        private kdService: KnowledgeDomainsService,
+        private mongoService: MongoService) {
 
         super();
 
         this.loadAllQuizTemplatesPromise = this.loadAllQuizTemplates();
 
     }
-
+    
     public ping() {
         this.clog.debug(`QuizTemplatesService: got a ping, pinging your right back.`);
         this.loadAllQuizTemplates();
@@ -35,10 +41,11 @@ export class QuizTemplatesService extends AbstractAngularService {
 
 
     public LoadAll() {
+        
         return this.loadAllQuizTemplatesPromise;
     }
 
-    public deleteQuizTemplateByUniqueID(forID: string): Promise<boolean> {
+    public deleteQuizTemplateByUniqueIDOLD(forID: string): Promise<boolean> {
 
         return new Promise<boolean>((resolve, reject) => {
             this.allQuizTemplates = this.allQuizTemplates.filter((anItem) => {
@@ -49,6 +56,24 @@ export class QuizTemplatesService extends AbstractAngularService {
 
             resolve(true);
         })
+
+    }
+
+    public deleteQuizTemplateByUniqueID(forID: string): Promise<boolean> {
+
+        return new Promise<boolean>(
+            async (resolve, reject) => {
+
+                const itemToDelete = await this.getQuizTemplateByUniqueID(forID);
+
+                const deleteResult = await this.mongoService.deleteItemG<QuizTemplateItem>(itemToDelete);
+
+                this.allQuizTemplates = this.allQuizTemplates.filter((anItem) => {
+                    return anItem.UniqueID !== forID;
+                });
+
+                resolve(deleteResult);
+            });
 
     }
 
@@ -75,22 +100,22 @@ export class QuizTemplatesService extends AbstractAngularService {
 
         this.clog.debug(`QT.Service: saveQuizTemplate: Entering, saving a QT:`, theQuizTemplate);
 
-        return new Promise((resolve, reject) => {
-            if (!Functionals.isIDAssigned(theQuizTemplate)) {
-                this.recordsService.getUniqueID().then(
-                    (newID) => {
-                        theQuizTemplate.UniqueID = newID;
-                        this.allQuizTemplates = Functionals.getMergedCollection(this.allQuizTemplates, theQuizTemplate);
-                        this.persistQuizTemplates();
-                        resolve(true);
-                    }
-                );
-            }
-            else {
-                this.allQuizTemplates = Functionals.getMergedCollection(this.allQuizTemplates, theQuizTemplate);
-                this.persistQuizTemplates();
-                resolve(true);
-            }
+        return new Promise(
+            async (resolve, reject) => {
+
+                if (!Functionals.isIDAssigned(theQuizTemplate)) {
+
+                    const saveResult = await this.mongoService.createItemG<QuizTemplateItem>(theQuizTemplate);
+
+                    theQuizTemplate.UniqueID = saveResult.UniqueID;
+                    this.allQuizTemplates = Functionals.getMergedCollection(this.allQuizTemplates, theQuizTemplate);
+                    resolve(true);
+                }
+                else {
+                    const saveResult = await this.mongoService.updateItemG<QuizTemplateItem>(theQuizTemplate);
+                    this.allQuizTemplates = Functionals.getMergedCollection(this.allQuizTemplates, theQuizTemplate);
+                    resolve(true);
+                }
         });
 
     }
@@ -99,10 +124,44 @@ export class QuizTemplatesService extends AbstractAngularService {
         localStorage.setItem(this.localStorageKey, JSON.stringify(this.allQuizTemplates));
     }
 
-    private loadAllQuizTemplates(): Promise<boolean> {
+    public async loadAllQuizTemplates(): Promise<boolean> {
+        
+        if (this.loadAllQuizTemplatesPromise) { 
+            
+            return this.loadAllQuizTemplatesPromise; 
+        }
+
+        // Need to load knowledge domains and facts first
+        await [this.kdService.getKnowledgeDomains(), this.factsService.loadAllFacts()];
+
+        this.loadAllQuizTemplatesPromise = new Promise<boolean>( 
+            async (resolve, reject) => {
+                
+                const rawQuizTemplates = await this.mongoService.loadAllItems<QuizTemplateItem>(QuizTemplateItem.GetEntityMetadata());
+
+                if (rawQuizTemplates) {
+                    this.allQuizTemplates = rawQuizTemplates.map((aQuizTemplate) => {
+                        return QuizTemplateItem.deNulled(aQuizTemplate);
+                    });
+                    this.clog.debug(`QT.Service: loadAllQuizTemplates: Got some quiz templates:`, this.allQuizTemplates);
+                    resolve(true);
+                }
+                else {
+                    this.allQuizTemplates = [];
+                    resolve(true);
+                }
+            }
+        )
+
+        return this.loadAllQuizTemplatesPromise;
+
+    }
+
+    private loadAllQuizTemplatesOLD(): Promise<boolean> {
 
         this.clog.debug(`QuizTemplatesService: Entering, retrieving all quiz templates from local storage.`);
 
+        
         return new Promise((resolve, reject) => {
             const rawData = localStorage.getItem(this.localStorageKey);
 
@@ -111,7 +170,7 @@ export class QuizTemplatesService extends AbstractAngularService {
             if (rawData) {
                 this.allQuizTemplates = JSON.parse(rawData);
                 this.allQuizTemplates = this.allQuizTemplates.map((aQuizTemplate) => {
-                    return QuizTemplateItem.initializeNulls(aQuizTemplate);
+                    return QuizTemplateItem.deNulled(aQuizTemplate);
                 });
                 this.clog.debug(`QT.Service: loadAllQuizTemplates: Got a some quiz templates:`, this.allQuizTemplates);
                 resolve(true);
